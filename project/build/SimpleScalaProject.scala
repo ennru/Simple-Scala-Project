4e47c4152d3a04a73981531bbdc6e0a35749d116
Enno Runne <enno@runne.net>
2010-10-29 21:41:53 +0200

    added sources and JavaDoc for dependencies

--- project/build/SimpleScalaProject.scala
+++ project/build/SimpleScalaProject.scala
@@ -4,9 +4,9 @@ import de.element34.sbteclipsify._
 class SimpleScalaProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify with IdeaProject
 {
   override def libraryDependencies = Set(
-    "org.scalatest" % "scalatest" % "1.2" % "test->default",
-    "junit" % "junit" % "4.8.2" % "test->default",
-    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default"
+    "org.scalatest" % "scalatest" % "1.2" % "test->default" withSources() withJavadoc(),
+    "junit" % "junit" % "4.8.2" % "test->default" withSources() withJavadoc(),
+    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default" withSources()
   ) ++ super.libraryDependencies
 	
 }
