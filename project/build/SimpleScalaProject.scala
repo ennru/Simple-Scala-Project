e2211ec6882b73bf2c296138a3dc813d9797af54
Enno Runne <enno@runne.net>
2011-05-13 22:24:00 +0200

    upgraded to Scala 2.9.0

--- project/build/SimpleScalaProject.scala
+++ project/build/SimpleScalaProject.scala
@@ -9,8 +9,10 @@ class SimpleScalaProject(info: ProjectInfo) extends DefaultProject(info) with Ec
   val scalatest = "org.scalatest" % "scalatest" % "1.3" withSources() withJavadoc()
   val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test"
   // imported by junitInterface  val junit = "junit" % "junit" % "4.8.1" % "test->default" withSources()
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.7.2" % "test->default" withSources()
-  val specs2 = "org.specs2" %% "specs2" % "1.0.1" % "test->default"
+ // val specs = "org.scala-tools.testing" %% "specs" % "1.6.7.2" % "test->default" withSources()
+
+  val specs2 = "org.specs2" %% "specs2" % "1.3"  
+  val scalaz = "org.specs2" %% "scalaz-core" % "6.0.RC2"
  
   def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
   override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
